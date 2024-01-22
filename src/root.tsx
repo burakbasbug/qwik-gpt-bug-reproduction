acc20ca55ca2e61f9198cbbdbbf01059d338bb17
Burak Basbug <burak.basbug@gmx.at>
2024-01-22 12:22:44 +0100

    add google tag manager example ad

--- src/root.tsx
+++ src/root.tsx
@@ -23,9 +23,32 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <QwikPartytown />
         <link rel="manifest" href="/manifest.json" />
+        <script type="text/partytown" async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js" />
+        <script type="text/partytown" dangerouslySetInnerHTML={`
+          window.googletag = window.googletag || { cmd: [] };
+
+          googletag.cmd.push(() => {
+            // Define an ad slot for div with id "banner-ad".
+            googletag
+              .defineSlot("/6355419/Travel/Europe/France/Paris", [300, 250], "banner-ad")
+              .addService(googletag.pubads());
+
+            // Enable the PubAdsService.
+            googletag.enableServices();
+          });
+        `} />
+          
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang="en" style="display: flex; justify-content: center; align-items: center; height: 800px; flex-direction: column;">
+        <h1>#banner-ad :</h1>
+        <div id="banner-ad" style="width: 300px; height: 250px; border: 1px solid black;"></div>
+        <script type="text/partytown" dangerouslySetInnerHTML={`
+          googletag.cmd.push(() => {
+            // Request and render an ad for the "banner-ad" slot.
+            googletag.display("banner-ad");
+          });
+        `} />
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
